14cd04bdd178cff0da139ae9c0c4c610a38ec553
Hnin Yee Maung Maung <hninyemaungmaung@gmail.com>
2025-07-13 21:38:02 +0200

    Add Test V1 API
    Add Test V1 API

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -9,4 +9,9 @@ public class HelloController {
     public String hello() {
         return "Hello from Spring Boot!";
     }
+    
+    @GetMapping("/v1/test")
+    public String helloV1() {
+        return "Hello Testingfrom Spring Boot!";
+    }
 }
